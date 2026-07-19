410afe227d334b4a37fece018d950cdf1ea9fcd5
turma198azul91 <turma198.azul91@gmail.com>
2026-07-19 14:07:13 -0400

    atualização de link

--- js/validacao.js
+++ js/validacao.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const URL_SCRIPT_GOOGLE = "https://script.google.com/macros/s/AKfycbxAGNqRGhe-9Gt-r7nCZxMD9LozLJofW2DY1cx_3WJAS2eOVNWdsNzrPz8R9B6Y4wdh/exec";
+    const URL_SCRIPT_GOOGLE = "https://script.google.com/macros/s/AKfycbxkipmzIhTGYl3lfCsogMWNbNKGWV8w29IsXizCVXK1PYEyKH-q9xzxI-TcpeqU2KpC/exec";
 
     // Elementos do Modal 1: Validação
     const modalValidacao = document.getElementById("modalValidacao");
