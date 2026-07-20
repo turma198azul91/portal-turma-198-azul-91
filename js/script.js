b8da34c644489a8628bbb66485d8613a1aff3fc9
turma198azul91 <turma198.azul91@gmail.com>
2026-07-20 13:23:54 -0400

    Mudança de Layout

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
     const buttonGrid = document.querySelector(".button-grid");
+    const content = document.querySelector(".content");
 
     if (menuToggle && buttonGrid) {
         menuToggle.addEventListener("click", function() {
             buttonGrid.classList.toggle("active");
+            content.classList.toggle("dimmed"); // Adiciona o efeito de esmaecer o fundo
+            
             const icon = menuToggle.querySelector("i");
             icon.className = buttonGrid.classList.contains("active") ? "fa-solid fa-xmark" : "fa-solid fa-bars";
         });
